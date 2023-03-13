b0f3dcb881f4354d05493e9ee29ffc1886ec9cc2
Charles Feval <fevalc>
2023-03-13 08:49:34 -0700

    Webhook

---
+++ PushHandler.js
@@ -0,0 +1,27 @@
+var exec = require('child_process').exec;
+
+export class PushHandler {
+  constructor(repos) {
+    this.repos = repos;
+    this.onPush = this.onPush.bind(this);
+  }
+
+  onPush(pushEvent) {
+    const repoName = pushEvent.repository.name;
+    const repo = this.repos[repoName];
+    const branch = pushEvent.repository.default_branch;
+    if (!repo) {
+      console.log(`Ignored push event as repo ${repoName} is not configured`);
+    }
+    const path = repo.path;
+
+    exec(`cd ${path} && git pull origin ${branch}`,
+      function (error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+              console.log('exec error: ' + error);
+          }
+      });
+  }
+}
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const fs = require("fs");
+const { PushHandler } = require("./PushHandler");
 
 const app = express();
 app.use(express.json());
 const config = JSON.parse(fs.readFileSync("config.json").toString());
+const handler = new PushHandler(config.repos);
 
 app.post("/api/push", (req, res) => {
   const body = req.body;
+  console.log(body.hook?.config?.secret);
+  const isPush = pushEvent.hook.events.indexOf("push") >= 0;
+  if (isPush) {
+    handler.onPush(body);
+  }
+  
   console.log(body);
 });
 
